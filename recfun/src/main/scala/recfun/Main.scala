1a57fac47a2e550d59bb1362e344dd3da1be1d26
a-nodi <leeyoonhyuk0@naver.com>
2024-09-11 17:20:01 +0900

    [Feat] Implemeted all of recursive functions

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -11,18 +11,44 @@ object Main {
     }
   }
 
-  /**
-   * Exercise 1
-   */
-  def pascal(c: Int, r: Int): Int = ???
+  /** Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || r == 0 || c == r)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
-  /**
-   * Exercise 2
-   */
-  def balance(chars: List[Char]): Boolean = ???
+  /** Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def iterateList(chars: List[Char], leftCount: Int, rightCount: Int): Boolean = {
+      if (chars.isEmpty)
+        leftCount == rightCount
+      else if (chars.head == '(')
+        iterateList(chars.tail, leftCount + 1, rightCount)
+      else if (chars.head == ')')
+        (leftCount > rightCount) && iterateList(chars.tail, leftCount, rightCount + 1)
+      else
+        iterateList(chars.tail, leftCount, rightCount)
+    }
 
-  /**
-   * Exercise 3
-   */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+    iterateList(chars, 0, 0)
+  }
+
+  /** Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def iterateCoins(money: Int, coins: List[Int]): Int = {
+      if (coins.isEmpty || money < 0) // Degenerated case 
+        0
+      else if (money == 0) // Solution 
+        1
+      else // Count out coin from money and reuse coin + count out no coin from money and eliminate coin
+        iterateCoins(money - coins.head, coins) + iterateCoins(money, coins.tail)
+    }
+
+    iterateCoins(money, coins)
+  }
 }
